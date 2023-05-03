3f25119d0f2e6595b73f0e758b092c5084e8357a
Diego Pérez <diegoperezperez518@gmail.com>
2023-05-02 19:51:37 -0600

    fixed typo on app.js

--- app.js
+++ app.js
@@ -10,12 +10,8 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 
-<<<<<<< HEAD
 // Parsing incoming requests with JSON payloads
 app.use(express.json());
-=======
-const port = process.env.PORT || 3000 || 8888 || 3100
->>>>>>> 71115d85b6dbfabd8a0354b2f302c147a1ef908a
 
 // Serving static files from the 'public' directory
 app.use(express.static(`public`));
@@ -29,7 +25,7 @@ app.use('/api/events', events.router);
 app.use('/api/students-in-events', studentsInEvents);
 
 // Setting the port number
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 3000 || 8888 | 3100;
 
 // Starting the server
 app.listen(port, () => console.log(`Listening on port ${port}`));
